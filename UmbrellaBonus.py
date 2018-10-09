f20ca017cd61028d1c211f53f9722ad1937e9034
chrivand <chrivand@cisco.com>
2018-10-09 09:50:24 -0700

    Add files via upload

--- UmbrellaBonus.py
+++ UmbrellaBonus.py
@@ -23,65 +23,65 @@ headers = {
 'limit': '1'
 }
 
-# retrieve text file with sans domains and write to .txt file
-result = requests.get("https://isc.sans.edu/feeds/suspiciousdomains_High.txt")
-open('domains.txt', 'wb').write(result.content)
+try:
+    # retrieve text file with sans domains and write to .txt file
+    result = requests.get("https://isc.sans.edu/feeds/suspiciousdomains_High.txt")
+    open('domains.txt', 'wb').write(result.content)
 
-# loop through .txt file and append every domain to list, skip comments
-domainList = []
-with open('domains.txt') as inputfile:
-    for line in inputfile:
-        if line[0] == "#" or line.strip() == "Site":
-            pass
+    # loop through .txt file and append every domain to list, skip comments
+    domainList = []
+    with open('domains.txt') as inputfile:
+        for line in inputfile:
+            if line[0] == "#" or line.strip() == "Site":
+                pass
+            else:
+                domainList.append(line.strip())
+
+    # time for AlertTime and EventTime when domains are added to Umbrella
+    time = datetime.now().isoformat()
+
+    # loop through all domains
+    for domain in domainList:
+
+        # assemble the URI, show labels give readable output
+        getUrl = investigateUrl + domain
+        # do GET request for the domain status and category
+        reqGet = requests.get(getUrl, headers=headers)
+
+        if(reqGet.status_code == 200):
+            # store threat grid json output in variable
+            output = reqGet.json()
+            if output["samples"] == []:
+                continue
+
+            sample = output["samples"][0]
+            hashSample = sample["sha256"]
+            scoreSample = sample["threatScore"]
+
+            # check if there is a threat grid sample with a score higher than or equal to 90, if so upload to custom block list
+            if scoreSample >= 90:
+
+                data = {
+                "alertTime": time + "Z",
+                "deviceId": "ba6a59f4-e692-4724-ba36-c28132c761de",
+                "deviceVersion": "13.7a",
+                "dstDomain": domain,
+                "dstUrl": "http://" + domain + "/",
+                "eventTime": time + "Z",
+                "protocolVersion": "1.0a",
+                "providerName": "Security Platform"
+                }
+
+                # POST REQUEST: post request ensembly
+                reqPost = requests.post(UrlPost, data=json.dumps(data), headers={'Content-type': 'application/json', 'Accept': 'application/json'})
+                if(reqPost.status_code == 202):
+                    print("\n")
+                    print("SUCCESS: The domain %(domain)s has an associated sample with Hash: %(hash)s and Threat Score: %(score)i at %(time)s" % {'domain': domain, 'hash': hashSample, 'score': scoreSample, 'time': time})
+                    print("\n")
+
         else:
-            domainList.append(line.strip())
+        	# error handling
+        	print("An error has ocurred with the following code %(error)s, please consult the following link: https://docs.umbrella.com/investigate-api/" % {'error': reqGet.status_code})
 
-# time for AlertTime and EventTime when domains are added to Umbrella
-time = datetime.now().isoformat()
-
-# loop through all domains
-for domain in domainList:
-
-    # assemble the URI, show labels give readable output
-    getUrl = investigateUrl + domain
-    # do GET request for the domain status and category
-    reqGet = requests.get(getUrl, headers=headers)
-
-    if(reqGet.status_code == 200):
-        # store threat grid json output in variable
-        output = reqGet.json()
-        # handle when there is no associated sample
-        if output["samples"] == []:
-            continue
-
-        # if there is a sample, grab some metadata
-        sample = output["samples"][0]
-        hashSample = sample["sha256"]
-        scoreSample = sample["threatScore"]
-
-        # check if there is a threat grid sample with a score higher than or equal to 90, if so upload to custom block list
-        if scoreSample >= 90:
-
-            # create "event" to upload to enforcement block list
-            data = {
-            "alertTime": time + "Z",
-            "deviceId": "ba6a59f4-e692-4724-ba36-c28132c761de",
-            "deviceVersion": "13.7a",
-            "dstDomain": domain,
-            "dstUrl": "http://" + domain + "/",
-            "eventTime": time + "Z",
-            "protocolVersion": "1.0a",
-            "providerName": "Security Platform"
-            }
-
-            # enforcement POST request
-            reqPost = requests.post(UrlPost, data=json.dumps(data), headers={'Content-type': 'application/json', 'Accept': 'application/json'})
-            # check if successful
-            if(reqPost.status_code == 202):
-                print("\n")
-                print("SUCCESS: The domain %(domain)s has an associated sample with Hash: %(hash)s and Threat Score: %(score)i at %(time)s" % {'domain': domain, 'hash': hashSample, 'score': scoreSample, 'time': time})
-                print("\n")
-
-    else:
-    	# error handling
-    	print("An error has ocurred with the following code %(error)s, please consult the following link: https://docs.umbrella.com/investigate-api/" % {'error': reqGet.status_code})
+except KeyboardInterrupt:
+    print("\nExiting...\n")
